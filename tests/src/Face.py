ec3a4fd3c8a9fe5988e31bcbfcae2a789b3186e6
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-05-25 20:49:00 +0300

    Added tests for triangles set and normals get

--- tests/src/Face.py
+++ tests/src/Face.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from numpy import array, allclose
+from numpy import array, allclose, array_equal
 
 from src import Face
 
@@ -29,4 +29,15 @@ class FaceTest(TestCase):
     def test_get_vertices(self):
         face = Face(self.vertices)
         self.assertTrue(allclose(face.get_vertices(), self.normalized_vertices))
+
+
+    def test_set_triangles(self):
+        Face.set_triangles(self.triangles, self.triangles_c)
+        self.assertTrue(array_equal(Face.get_triangles(), self.triangles))
+        self.assertEqual(Face.get_triangles_c(), self.triangles_c)
+
+
+    def test_get_normals(self):
+        face = Face(self.vertices)
+        self.assertTrue(allclose(face.get_normals(), 3.**(-.5)))
 
