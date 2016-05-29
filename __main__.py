aebfe344388eb826956cbd3bf205e50fc5bb4d85
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-29 13:18:17 +0300

    Made constants names uppercase

--- __main__.py
+++ __main__.py
@@ -2,16 +2,16 @@ from load_model import load_model
 from render_face import render_face
 from src import Face
 
-mfm = load_model()
+MFM = load_model()
 
-model = {
-    'mfm': mfm,
+MODEL = {
+    'mfm': MFM,
     'light': True,
     'face': None
 }
-triangles = mfm['tl'] - 1
-triangles_flattened = (mfm['tl'] - 1).flatten()
-Face.set_triangles(triangles, triangles_flattened.ctypes.get_as_parameter())
+TRIANGLES = MFM['tl'] - 1
+TRIANGLES_FLATTENED = (MFM['tl'] - 1).flatten()
+Face.set_triangles(TRIANGLES, TRIANGLES_FLATTENED.ctypes.get_as_parameter())
 
-render_face(model)
+render_face(MODEL)
 
