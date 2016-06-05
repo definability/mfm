e67a95fd298f6de38aa7f3584db35933485e0d97
char-lie <5962274+char-lie@users.noreply.github.com>
2016-06-05 14:59:49 +0300

    Added C function for normal vectors calculation
    The function calculates cross products of edges for given triangles
    
    Result is an array of normal vectors for each triangle

--- cross.c
+++ cross.c
@@ -2,6 +2,35 @@
 #include <stdint.h>
 #include <math.h>
 
+void cross(float* vertices, uint16_t* triangles, float* result,
+           int amount) {
+    size_t u, v, w;
+    int i = 0;
+
+    do {
+        u = 3 * ((size_t)triangles[3*i + 1]);
+        v = 3 * ((size_t)triangles[3*i + 2]);
+        w = 3 * ((size_t)triangles[3*i + 0]);
+
+        result[3*i]     = (vertices[u+1] - vertices[w+1]) *
+                          (vertices[v+2] - vertices[w+2]) -
+                          (vertices[u+2] - vertices[w+2]) *
+                          (vertices[v+1] - vertices[w+1]);
+
+        result[3*i + 1] = (vertices[u+2] - vertices[w+2]) *
+                          (vertices[v+0] - vertices[w+0]) -
+                          (vertices[u+0] - vertices[w+0]) *
+                          (vertices[v+2] - vertices[w+2]);
+
+        result[3*i + 2] = (vertices[u+0] - vertices[w+0]) *
+                          (vertices[v+1] - vertices[w+1]) -
+                          (vertices[u+1] - vertices[w+1]) *
+                          (vertices[v+0] - vertices[w+0]);
+
+        i++;
+    } while (i < amount);
+}
+
 void normals(float* normal_vectors, uint16_t* triangles, float* result,
              int amount) {
     uint16_t triangle;
