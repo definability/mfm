a5fd1c73d8ec7e2a7b07066f88b85a7119970828
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-06-06 23:58:00 +0300

    Implemented Model Fitter class

---
+++ src/ModelFitter.py
@@ -0,0 +1,45 @@
+from numpy import array, nonzero, ones_like
+from numpy.linalg import lstsq, norm
+
+
+class ModelFitter:
+    def __init__(self, image):
+        self.__image = image
+
+    def check(self, normals):
+        #indices = nonzero(norm(normals, axis=1))
+        indices = normals[:, 3] > 0.5
+
+        N = normals[indices]
+        Y = self.__image[indices]
+
+        N_x = N[:, 0]
+        N_y = N[:, 1]
+        N_z = N[:, 2]
+
+        y_x = Y.dot(N_x)
+        y_y = Y.dot(N_y)
+        y_z = Y.dot(N_z)
+
+        n_x = N_x.sum()
+        n_y = N_y.sum()
+        n_z = N_z.sum()
+
+        n_xx = N_x.dot(N_x)
+        n_xy = N_x.dot(N_y)
+        n_xz = N_x.dot(N_z)
+        n_yy = N_y.dot(N_y)
+        n_yz = N_y.dot(N_z)
+        n_zz = N_z.dot(N_z)
+
+        A = array([
+            [n_xx, n_xy, n_xz, n_x   ],
+            [n_xy, n_yy, n_yz, n_y   ],
+            [n_xz, n_yz, n_zz, n_z   ],
+            [n_x,  n_y,  n_z,  len(N)],
+        ])
+        y = array([y_x, y_y, y_z, Y.sum()])
+
+        x, residuals, rank, s = lstsq(A, y)
+
+        return x
