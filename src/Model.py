64e6ec78f38d924d2b51ac077e8ce761098a618e
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-06-15 22:37:21 +0300

    Implemented image and normal vectors saving in Model

--- src/Model.py
+++ src/Model.py
@@ -1,6 +1,8 @@
 from enum import Enum
 
 from OpenGL.GLUT import glutMainLoop
+from PIL import Image
+from numpy import save, array, column_stack
 
 from src import MFM
 
@@ -72,3 +74,23 @@ class Model:
 
     def close(self):
         self.__view.close()
+
+    def save_image(self, filename):
+        if self.__texture == Texture.normal:
+            img = array(self.__view.get_image())
+            img = img.reshape(img.size//4, 4)
+            data = column_stack((
+                    self.__face.normal_map_to_normal_vectors(img[:, :3]),
+                    img[:, 3]))
+            with open(filename + '.npy', 'wb') as f:
+                save(f, data[::-1])
+        elif self.__texture == Texture.light:
+            size = self.__view.get_size()
+            image = Image.new('RGBA', size)
+            data = (self.__view.get_image() * 255).astype('i')
+            pixels = [(data[i*4+0], data[i*4+1], data[i*4+2], data[i*4+3])
+                       for i in range(size[0]*size[1]-1, -1, -1)]
+            image.putdata(pixels)
+            image.save(filename + '.png')
+            image.close()
+            print(*self.__face.get_directed_light(), self.__face.get_constant_light())
