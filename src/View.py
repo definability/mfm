3848dddd77090d72f8c1469483360060f4d98e7a
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-06-15 22:32:10 +0300

    Enabled face culling in View

--- src/View.py
+++ src/View.py
@@ -4,9 +4,11 @@ from OpenGL.GL import GL_LESS, GL_TRUE, GL_DEPTH_TEST, GL_STENCIL_TEST
 from OpenGL.GL import GL_COLOR_ARRAY, GL_VERTEX_ARRAY, GL_TRIANGLES
 from OpenGL.GL import GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT
 from OpenGL.GL import GL_UNSIGNED_SHORT, GL_FLOAT, GL_MODELVIEW
+from OpenGL.GL import GL_CULL_FACE, GL_FRONT
 
 from OpenGL.GL import glMatrixMode, glLoadIdentity, glOrtho, glRotatef
-from OpenGL.GL import glDepthMask, glDepthFunc, glVertexPointer, glColorPointer
+from OpenGL.GL import glDepthMask, glDepthFunc, glCullFace
+from OpenGL.GL import glVertexPointer, glColorPointer
 from OpenGL.GL import glEnable, glClearColor, glEnableClientState, glClear
 from OpenGL.GL import glDrawElements
 
@@ -99,6 +101,9 @@ class View:
     def __enable_depth_test(self):
         glDepthMask(GL_TRUE)
         glDepthFunc(GL_LESS)
+
+        glEnable(GL_CULL_FACE)
+        glCullFace(GL_FRONT)
 
         glEnable(GL_DEPTH_TEST)
         glEnable(GL_STENCIL_TEST)
