d4f19a260c8bbcfd150cf814015af4c670ba4bd4
char-lie <5962274+char-lie@users.noreply.github.com>
2016-06-19 20:21:44 +0300

    Added Fitter instance on __main__ launch

--- __main__.py
+++ __main__.py
@@ -1,7 +1,18 @@
-from src import MFM, Model, ModelInput, View
+from PIL import Image
+from numpy import array
+
+from src import MFM, Model, ModelInput, View, ModelFitter
+from data import get_datafile_path
 
 MFM.init()
 view = View((500, 500))
 model = Model(view)
 model_input = ModelInput(model)
-model.start()
+
+model_filename = get_datafile_path('test.png')
+image = Image.open(model_filename).convert('L')
+fitter = ModelFitter(image=array((image.getdata()))[::-1].astype('f') / 255,
+                     model=model)
+image.close()
+
+model.start(fitter)
