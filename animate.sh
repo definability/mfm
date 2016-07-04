4e59b9097c27c68dae888c20b12ab0461a823fee
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-07-04 11:20:44 +0300

    Refactored animation creation script

--- animate.sh
+++ animate.sh
@@ -1,4 +1,8 @@
 mplayer -ao null out.ogv -vo jpeg:outdir=output
-for i in out/*.jpg; do convert $i -crop 360x115+140+160 anim/$(basename $i); done;
-convert anim/* out.gif 
 
+for i in out/*.jpg
+do
+    convert $i -crop 360x115+140+160 anim/$(basename $i)
+done
+
+convert anim/* out.gif
