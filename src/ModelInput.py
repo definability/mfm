7f41f1c6bcec22156c78a7df0757db3c79ce5239
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-07-04 11:32:52 +0300

    Refactored ModelInput
    Fixed typo in methods names: replaced special and alphanumerical keys
    handlers;
    Merged dictionaries with rotation keys.
    Added docstrings.

--- src/ModelInput.py
+++ src/ModelInput.py
@@ -9,13 +9,12 @@ from OpenGL.GLUT import glutKeyboardFunc
 
 
 class ModelInput:
-    __special_keys = {
+    """Process input and trigger needed methods of Model."""
+    __rotation_keys = {
         GLUT_KEY_UP: ('x', -1.),
         GLUT_KEY_DOWN: ('x', 1.),
         GLUT_KEY_RIGHT: ('y', -1.),
-        GLUT_KEY_LEFT: ('y', 1.)
-    }
-    __keys = {
+        GLUT_KEY_LEFT: ('y', 1.),
         b'z': ('z', -1.),
         b'a': ('z', 1.)
     }
@@ -27,23 +26,26 @@ class ModelInput:
     }
 
     def __init__(self, model):
+        """Initialize input handlers and link given Model to the Input."""
         self.__model = model
 
-        glutKeyboardFunc(lambda key, x, y: self.__handle_special(key, False))
-        glutKeyboardUpFunc(lambda key, x, y: self.__handle_special(key, True))
-        glutSpecialFunc(lambda key, x, y: self.__handle_key(key, False))
-        glutSpecialUpFunc(lambda key, x, y: self.__handle_key(key, True))
+        glutKeyboardFunc(lambda key, x, y: self.__handle_key(key, False))
+        glutKeyboardUpFunc(lambda key, x, y: self.__handle_key(key, True))
+        glutSpecialFunc(lambda key, x, y: self.__handle_special(key, False))
+        glutSpecialUpFunc(lambda key, x, y: self.__handle_special(key, True))
 
-    def __handle_key(self, key, release=False):
-        if key in ModelInput.__special_keys:
-            self.__rotate(ModelInput.__special_keys, key, release)
+    def __handle_special(self, key, release=False):
+        """Process special keys of the keyboard."""
+        if key in ModelInput.__rotation_keys:
+            self.__rotate(ModelInput.__rotation_keys, key, release)
         else:
             return
         self.__model.redraw()
 
-    def __handle_special(self, key, release=False):
-        if key in ModelInput.__keys:
-            self.__rotate(ModelInput.__keys, key, release)
+    def __handle_key(self, key, release=False):
+        """Process alphanumerical keys of the keyboard."""
+        if key in ModelInput.__rotation_keys:
+            self.__rotate(ModelInput.__rotation_keys, key, release)
         elif key in ModelInput.__light_keys and not release:
             directed_light = {
                 'x': 0.,
@@ -76,6 +78,7 @@ class ModelInput:
         self.__model.redraw()
 
     def __rotate(self, commands, key, release):
+        """Process rotation keys."""
         assert key in commands
         axis, value = commands[key]
         self.__model.rotate(axis, 0. if release else value)
