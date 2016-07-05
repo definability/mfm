01edf437006d1d03c159ecc8d759a3621fc90b76
char-lie <5962274+char-lie@users.noreply.github.com>
2016-07-05 20:07:15 +0300

    Closes #22

--- src/fitter/__init__.py
+++ src/fitter/__init__.py
@@ -1,5 +1,7 @@
 from .ModelFitter import ModelFitter
 from .NelderMeadFitter import NelderMeadFitter
 from .GibbsSamplerFitter import GibbsSamplerFitter
+from .BruteForceFitter import BruteForceFitter
 
-__all__ = ['ModelFitter', 'NelderMeadFitter', 'GibbsSamplerFitter']
+__all__ = ['ModelFitter', 'NelderMeadFitter', 'GibbsSamplerFitter',
+           'BruteForceFitter']
