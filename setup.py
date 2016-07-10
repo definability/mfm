aee7a10c5534415a4e81d7da0ce168109044f3d4
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-07-10 14:46:52 +0300

    Generalized build procedure
    Created function to get C function and wrapper path.
    Joining path in crossplatform way via `os.path.join`.

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from distutils.core import setup, Extension
+from os.path import join
 import numpy.distutils.misc_util
 
-normals = Extension("src.normals", ["libs/_normals.c", "libs/normals.c"])
-face = Extension("src.face", ["libs/_face.c", "libs/face.c"])
+SOURCES_DIR = 'libs'
+C_EXTENSION = '.c'
+WRAPPER_PREFIX = '_'
+
+def get_sources(name):
+    filename = name + C_EXTENSION
+    return [join(SOURCES_DIR, n) for n in [filename, WRAPPER_PREFIX + filename]]
+
+normals = Extension('src.normals', get_sources('normals'))
+face = Extension('src.face', get_sources('face'))
 
 setup(ext_modules=[normals, face],
       include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
