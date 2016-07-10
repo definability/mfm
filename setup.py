e61fd504898aa72c70c49693300d28bc02a36dcf
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-07-10 20:58:19 +0300

    Using setuptools instead of distutil
    Building package with
    - main info like name, version, description
    - internal C modules dependencies
    - external dependencies
    - test command

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from distutils.core import setup, Extension
-from os.path import join
+from setuptools import Extension, setup, find_packages
+from os.path import join, dirname
 import numpy.distutils.misc_util
 
 SOURCES_DIR = 'libs'
@@ -13,5 +13,47 @@ def get_sources(name):
 normals = Extension('src.normals', get_sources('normals'))
 face = Extension('src.face', get_sources('face'))
 
-setup(ext_modules=[normals, face],
-      include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
+long_description=''
+with open(join(dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    ext_modules=[normals, face],
+    include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
+    name='MFM',
+    version='0.0.14',
+    license='MIT',
+    description='Morphable Face Model fitting application',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    packages=[join('src', p) for p in find_packages('src')] + \
+             [join('tests', p) for p in find_packages('tests')] + \
+             ['data'],
+    install_requires=[
+        'argparse==1.2.1',
+        'cairocffi==0.7.2',
+        'enum34==1.1.6',
+        'Pillow==3.2.0',
+        'PyOpenGL==3.1.0',
+    ],
+    extras_require={
+        'dev': [
+            'flake8==2.6.2',
+            'pylint==1.5.5'
+        ],
+        'test': [
+            'coverage==4.0',
+            'coveralls==1.1'
+        ]
+    },
+    test_suite='tests'
+)
