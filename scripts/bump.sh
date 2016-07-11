54422965a4b64649526217b53aa7cbcd5d6af71d
char-lie <5962274+char-lie@users.noreply.github.com>
2016-07-11 11:58:19 +0300

    Fixed bump script: avoiding trailing space

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -14,20 +14,22 @@ NEW_VERSION=$(sed "s/${VERSION_REGEX}/${BUMPER}/ge" <<< ${VERSION})
 
 TIMESTAMP=$(date +%Y-%m-%d)
 CHANGELOG_ENTRY="## [${NEW_VERSION}] - ${TIMESTAMP}"
-sed -i "/^## \[Unreleased\]$/a\ \n${CHANGELOG_ENTRY}" CHANGELOG.md
+sed "/^## \[Unreleased\]$/a\
 
-CHANGELOG_HEAD_LINK="^\[Unreleased\]\(.*\)v${VERSION}...HEAD$"
-CHANGELOG_NEW_HEAD_LINK="[Unreleased]\1v${NEW_VERSION}...HEAD"
-CHANGELOG_NEW_COMPARISON="[${NEW_VERSION}]\1v${VERSION}...${NEW_VERSION}"
-CHANGELOG_LINK="${CHANGELOG_NEW_HEAD_LINK}\n${CHANGELOG_NEW_COMPARISON}"
-sed -i "s/${CHANGELOG_HEAD_LINK}/${CHANGELOG_LINK}/g" CHANGELOG.md
+${CHANGELOG_ENTRY}" CHANGELOG.md
 
-sed -i "s/version='${VERSION}'/version='${NEW_VERSION}'/g" setup.py
-
-git add CHANGELOG.md setup.py
-git commit -m "Version ${NEW_VERSION}"
-git tag "v${NEW_VERSION}"
-git push
-git push --tags
-
-exit 0
+#CHANGELOG_HEAD_LINK="^\[Unreleased\]\(.*\)v${VERSION}...HEAD$"
+#CHANGELOG_NEW_HEAD_LINK="[Unreleased]\1v${NEW_VERSION}...HEAD"
+#CHANGELOG_NEW_COMPARISON="[${NEW_VERSION}]\1v${VERSION}...${NEW_VERSION}"
+#CHANGELOG_LINK="${CHANGELOG_NEW_HEAD_LINK}\n${CHANGELOG_NEW_COMPARISON}"
+#sed -i "s/${CHANGELOG_HEAD_LINK}/${CHANGELOG_LINK}/g" CHANGELOG.md
+#
+#sed -i "s/version='${VERSION}'/version='${NEW_VERSION}'/g" setup.py
+#
+#git add CHANGELOG.md setup.py
+#git commit -m "Version ${NEW_VERSION}"
+#git tag "v${NEW_VERSION}"
+#git push
+#git push --tags
+#
+#exit 0
