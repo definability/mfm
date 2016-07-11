e7ba2670834099a887c772b6f9443ae4a0a444d1
char-lie <5962274+char-lie@users.noreply.github.com>
2016-07-11 10:46:31 +0300

    Added ability to control bump type
    Accepting first parameter for patch, minor and major version update.
    Based on it the script increases the version.

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,3 +1,5 @@
+OPERATION=$(tr [a-z] [A-Z] <<< ${1:-patch})
+
 VERSION=$(python setup.py --version)
 
 REGEX='^\([[:digit:]]\+\)\.\([[:digit:]]\+\)\.\([[:digit:]]\+\)$'
@@ -5,3 +7,7 @@ REGEX='^\([[:digit:]]\+\)\.\([[:digit:]]\+\)\.\([[:digit:]]\+\)$'
 PATCH='echo "\1.\2.$((\3+1))"'
 MINOR='echo "\1.$((\2+1)).0"'
 MAJOR='echo "$((\1+1)).0.0"'
+
+eval BUMPER='$'$OPERATION
+
+NEW_VERSION=$(sed "s/${REGEX}/${BUMPER}/ge" <<< ${VERSION})
