fd48687f9fcba6b9557778adc7d13cc85f475d5f
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-07-11 10:45:47 +0300

    Added bump methods to bump script
    Implemented regex to parse version.
    Implemented replacers for version bump.

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,1 +1,7 @@
 VERSION=$(python setup.py --version)
+
+REGEX='^\([[:digit:]]\+\)\.\([[:digit:]]\+\)\.\([[:digit:]]\+\)$'
+
+PATCH='echo "\1.\2.$((\3+1))"'
+MINOR='echo "\1.$((\2+1)).0"'
+MAJOR='echo "$((\1+1)).0.0"'
