10d6047017ae4db6ac6b5ef308f2f3505d480dad
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-07-11 11:24:36 +0300

    Refactored bump script
    Grouped bump operations and renamed variable with regex.

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -2,15 +2,12 @@ OPERATION=$(tr [a-z] [A-Z] <<< ${1:-patch})
 
 VERSION=$(python setup.py --version)
 
-REGEX='^\([[:digit:]]\+\)\.\([[:digit:]]\+\)\.\([[:digit:]]\+\)$'
-
+VERSION_REGEX='^\([[:digit:]]\+\)\.\([[:digit:]]\+\)\.\([[:digit:]]\+\)$'
 PATCH='echo "\1.\2.$((\3+1))"'
 MINOR='echo "\1.$((\2+1)).0"'
 MAJOR='echo "$((\1+1)).0.0"'
-
 eval BUMPER='$'$OPERATION
-
-NEW_VERSION=$(sed "s/${REGEX}/${BUMPER}/ge" <<< ${VERSION})
+NEW_VERSION=$(sed "s/${VERSION_REGEX}/${BUMPER}/ge" <<< ${VERSION})
 
 TIMESTAMP=$(date +%Y-%m-%d)
 CHANGELOG_ENTRY="## [${NEW_VERSION}] - ${TIMESTAMP}"
