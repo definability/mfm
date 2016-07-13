53c2419cb10d36f40aae0dee0b0407f52f1a7794
char-lie <5962274+char-lie@users.noreply.github.com>
2016-07-13 22:21:54 +0300

    Using SGD Fitter in main application

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ from PIL import Image
 from numpy import array
 
 from src import MFM, Model, ModelInput, View
-from src.fitter import BruteForceFitter as Fitter
+from src.fitter import StochasticGradientDescentFitter as Fitter
 from data import get_datafile_path
 
 MFM.init()
@@ -13,8 +13,8 @@ model_input = ModelInput(model)
 model_filename = get_datafile_path('test.png')
 image = Image.open(model_filename).convert('L')
 fitter = Fitter(image=array((image.getdata()))[::-1].astype('f') / 255,
-                model=model, dimensions=199, steps=MFM.get_multipliers(3),
-                max_level=2)
+                model=model, dimensions=100, max_loops=100,
+                dx=.1, step=100.)
 image.close()
 
 model.start(fitter)
