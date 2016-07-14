73a3abcddf679656bd6452d110ee10fb165ac2f0
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-07-14 21:00:25 +0300

    Added ability to use not all principal components
    There is 199 dimensions by default, but not all principal components are
    needed during optimization and it costs a lot to concatenate array of
    zeros to existent array with coefficients.
    
    It was decided to suppose, that all unset coefficients are set to zero
    and not iterate them during Face calculation in C functions.

--- libs/_face.c
+++ libs/_face.c
@@ -56,13 +56,14 @@ static PyObject* _calculate_face(PyObject *self, PyObject *args) {
     dims[1] = 1;
     result = (PyArrayObject*)PyArray_ZEROS(2, dims, NPY_FLOAT, 0);
 
+    size_t coefficients_size = PyArray_SIZE(coefficients);
     calculate_face(
         mean_shape,
         principal_components,
         pc_deviations,
         PyArray_DATA(coefficients),
         PyArray_DATA(result),
-        dimensions,
+        dimensions < coefficients_size ? dimensions : coefficients_size,
         vertices);
 
     return PyArray_Return(result);
