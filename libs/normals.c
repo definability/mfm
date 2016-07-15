6f565cb4ce6ebc19d57f469cbefdd2a32e0a5397
char-lie <5962274+char-lie@users.noreply.github.com>
2016-07-15 10:43:51 +0300

    Refactored C modules for normals processing
    Using size_t instead of int.
    Avoided redundant variable `i` for indexing &mdash;
    just decrement total count and using it as an index.

--- libs/normals.c
+++ libs/normals.c
@@ -11,16 +11,15 @@
 #define Y_OFFSET 1
 #define Z_OFFSET 2
 
-void cross(float* vertices, uint16_t* triangles, float* result, int amount) {
+void cross(float* vertices, uint16_t* triangles, float* result, size_t amount) {
     size_t u, v, w;
     size_t triangle;
     float ux, uy, uz;
     float vx, vy, vz;
     float wx, wy, wz;
-    int i = 0;
 
-    do {
-        triangle = COMPONENTS_COUNT*i;
+    while (amount --> 0) {
+        triangle = COMPONENTS_COUNT*amount;
 
         u = COMPONENTS_COUNT * ((size_t)triangles[triangle + 0]);
         v = COMPONENTS_COUNT * ((size_t)triangles[triangle + 1]);
@@ -41,53 +40,52 @@ void cross(float* vertices, uint16_t* triangles, float* result, int amount) {
         result[triangle + X_OFFSET] = uy*vz - uz*vy;
         result[triangle + Y_OFFSET] = uz*vx - ux*vz;
         result[triangle + Z_OFFSET] = ux*vy - uy*vx;
-    } while (++i < amount);
+    }
 }
 
 void normals(float* normal_vectors, uint16_t* triangles, float* result,
-             int amount) {
+             size_t amount) {
     uint16_t vertex;
     float normal_x, normal_y, normal_z;
-    int i = 0;
-    int j;
+    size_t j;
 
-    do {
-        j = 0;
-        normal_x = normal_vectors[i*COMPONENTS_COUNT + X_OFFSET];
-        normal_y = normal_vectors[i*COMPONENTS_COUNT + Y_OFFSET];
-        normal_z = normal_vectors[i*COMPONENTS_COUNT + Z_OFFSET];
-        do {
-            vertex = triangles[i*COMPONENTS_COUNT + j];
+    while (amount --> 0) {
+        normal_x = normal_vectors[amount*COMPONENTS_COUNT + X_OFFSET];
+        normal_y = normal_vectors[amount*COMPONENTS_COUNT + Y_OFFSET];
+        normal_z = normal_vectors[amount*COMPONENTS_COUNT + Z_OFFSET];
+
+        j = COMPONENTS_COUNT;
+        while (j --> 0) {
+            vertex = triangles[amount*COMPONENTS_COUNT + j];
             result[vertex*COMPONENTS_COUNT + X_OFFSET] += normal_x;
             result[vertex*COMPONENTS_COUNT + Y_OFFSET] += normal_y;
             result[vertex*COMPONENTS_COUNT + Z_OFFSET] += normal_z;
-        } while (++j < COMPONENTS_COUNT);
-    } while (++i < amount);
+        }
+    }
 }
 
-void normalize(float* normals, int amount) {
+void normalize(float* normals, size_t amount) {
     float norm;
-    int i = 0;
     size_t normal_x, normal_y, normal_z;
 
-    do {
-        normal_x = i*COMPONENTS_COUNT + X_OFFSET;
-        normal_y = i*COMPONENTS_COUNT + Y_OFFSET;
-        normal_z = i*COMPONENTS_COUNT + Z_OFFSET;
+    while (amount --> 0) {
+        normal_x = amount*COMPONENTS_COUNT + X_OFFSET;
+        normal_y = amount*COMPONENTS_COUNT + Y_OFFSET;
+        normal_z = amount*COMPONENTS_COUNT + Z_OFFSET;
 
-        norm = sqrt(normals[normal_x] * normals[normal_x]
+        norm = sqrt(  normals[normal_x] * normals[normal_x]
                     + normals[normal_y] * normals[normal_y]
                     + normals[normal_z] * normals[normal_z]);
         normals[normal_x] /= norm;
         normals[normal_y] /= norm;
         normals[normal_z] /= norm;
-    } while (++i < amount);
+    }
 }
 
 void get_normals(float* vertices, uint16_t* triangles, float* normal_map,
-                 int vertices_count, int triangles_count) {
-    float* normal_vectors = (float*)malloc(COMPONENTS_COUNT *
-                                           sizeof(float) * triangles_count);
+                 size_t vertices_count, size_t triangles_count) {
+    float* normal_vectors = (float*)malloc(
+        COMPONENTS_COUNT * sizeof(float) * triangles_count);
     assert(normal_vectors);
     cross(vertices, triangles, normal_vectors, triangles_count);
     normals(normal_vectors, triangles, normal_map, triangles_count);
