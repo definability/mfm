36022ae3e84cb67bec1a66bd4f78a86b6b90d37d
char-lie <5962274+char-lie@users.noreply.github.com>
2016-08-06 21:17:27 +0300

    Fixed code due to Shaders Helper move

--- shaders/__init__.py
+++ shaders/__init__.py
@@ -1,1 +1,7 @@
-from .ShadersHelper import ShadersHelper
+from os.path import dirname, abspath, join
+
+SHADERS_FOLDER = dirname(abspath(__file__))
+
+
+def get_shader_path(filename):
+    return join(SHADERS_FOLDER, filename)
--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,8 @@ from .Face import Face
 from .View import View
 from .Model import Model
 from .ModelInput import ModelInput
+from .ShadersHelper import ShadersHelper
 from . import fitter
 
-__all__ = ['MFM', 'Face', 'View', 'Model', 'ModelInput', 'fitter']
+__all__ = ['MFM', 'Face', 'View', 'Model', 'ModelInput', 'ShadersHelper',
+           'fitter']
