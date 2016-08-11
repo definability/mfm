90310c885317cf945332336d27aac496c76c99b7
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-08-11 22:24:18 +0300

    Added example with usage of Brute Force Fitter

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ from PIL import Image
 from numpy import array
 
 from src import MFM, Model, ModelInput, View
-from src.fitter import BGDFitter as Fitter
+from src.fitter import BruteForceFitter as Fitter
 from data import get_datafile_path
 
 MFM.init()
@@ -12,9 +12,13 @@ model_input = ModelInput(model)
 
 model_filename = get_datafile_path('test.png')
 image = Image.open(model_filename).convert('L')
-fitter = Fitter(image=array((image.getdata()))[::-1].astype('f') / 255,
-                model=model, dimensions=100, max_loops=100,
-                dx=.1, step=100.)
+image_data = array((image.getdata()))[::-1].astype('f') / 255
 image.close()
+
+fitter = Fitter(
+    image=image_data, model=model, dimensions=0,
+    steps=[1, 10, 10, 5], max_level=4,
+    offsets=[0, -0.5, -0.5, 0],
+    scale=[0, -2, -2, 1])
 
 model.start(fitter)
