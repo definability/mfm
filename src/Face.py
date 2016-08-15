b2b0b6ad8856aeb44080608836db625535ad1465
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-08-15 21:23:40 +0300

    Removed deprecated methods from Face class

--- src/Face.py
+++ src/Face.py
@@ -18,141 +18,26 @@ ERROR_TEXT = {
 }
 
 
-def normalize(vertices):
-    warn('Shaders will zoom it', DeprecationWarning)
-    vertices = vertices.reshape(vertices.size // 3, 3)
-    vertices = vertices - vertices.min()
-    vertices /= vertices.max()
-    vertices -= apply_along_axis(mean, 0, vertices)
-    return vertices
-
-
 class Face:
 
     __triangles = None
     __triangles_flattened = None
     __triangles_c = None
 
-    def __init__(self, vertices, directed_light=None, constant_light=0,
-                 coefficients=None, need_normalize=True):
-        """Create new Face.
+    def __init__(self, ambient_light=0, directed_light=None,
+                 coefficients=None):
+        """Create new Face."""
+        self.ambient_light = ambient_light
 
-        Vertices are connected by triangles.
-        Coefficients needed to generate new Face based on existent one,
-        if we need to change few parameters and it will be more efficient
-        than recalculate new Face.
-        """
-        if vertices.size % 3 != 0:
-            raise ValueError(ERROR_TEXT['VERTICES_SIZE'].format(vertices.size))
-        vertices = vertices.astype('f')
-        self.__original_vertices = vertices.copy()
-        if need_normalize:
-            self.__vertices = normalize(vertices)
+        if directed_light is None:
+            self.directed_light = zeros(3, dtype='f')
         else:
-            self.__vertices = vertices.reshape(vertices.size // 3, 3)
-        self.__vertices_c = self.__vertices.ctypes.get_as_parameter()
-        self.__light_map = None
-        self.__directed_light = None
-        self.__constant_light = constant_light
-        if directed_light is not None:
-            self.set_light(directed_light, constant_light)
-        self.__coefficients = coefficients
+            self.directed_light = directed_light
 
-        self.__normals = None
-        self.__normal_map = None
-
-        self.__normal_max = None
-        self.__normal_min = None
-
-    def get_coefficients(self):
-        """Get coefficients for eigenvectors of current Face."""
-        warn('Use property instead', DeprecationWarning)
-        return self.__coefficients
-
-    def get_original_vertices(self):
-        """Get not normalized vertices.
-
-        Handy for creation of new Face based on this one.
-        """
-        warn('Will be calculated by shaders', DeprecationWarning)
-        return self.__original_vertices
-
-    def get_vertices(self):
-        """Get normalized vertices."""
-        warn('Will be calculated by shaders', DeprecationWarning)
-        return self.__vertices
-
-    def get_vertices_c(self):
-        """Get C array of normalized vertices."""
-        warn('Shaders don\'t need C array', DeprecationWarning)
-        return self.__vertices_c
-
-    @staticmethod
-    def get_triangles_c():
-        """Get C array of indices of vertices connected in triangles."""
-        warn('Shaders don\'t need C array', DeprecationWarning)
-        return Face.__triangles_c
-
-    @staticmethod
-    def get_triangles():
-        """Get NumPy matrix indices of vertices connected in triangles."""
-        return Face.__triangles
-
-    def get_directed_light(self):
-        """Get directed light vector."""
-        warn('Use property instead', DeprecationWarning)
-        return self.__directed_light
-
-    def get_constant_light(self):
-        """Get ambient light intencity."""
-        warn('Use property ambient_light instead', DeprecationWarning)
-        return self.__constant_light
-
-    def get_light_map(self):
-        """Get array with intensities of vertices.
-
-        Dot product of normal vectors and directed light calculated
-        and cropped to [0; 1] bounds.
-        Then ambient light is added and cropped again.
-
-        Result contains array of 3-arrays for each of RGB channel.
-        """
-        warn('Shadows are calculated by shaders', DeprecationWarning)
-        if self.__light_map is not None:
-            return self.__light_map
-
-        self.__light_map = dot(self.get_normals(), self.__directed_light)
-        self.__light_map[self.__light_map < 0.] = 0.
-        self.__light_map += self.__constant_light
-        self.__light_map[self.__light_map > 1.] = 1.
-        self.__light_map = column_stack([self.__light_map.astype('f')]*3)
-
-        return self.__light_map
-
-    def get_light_map_c(self):
-        """Get C array with light map."""
-        warn('Shadows are calculated by shaders', DeprecationWarning)
-        return self.get_light_map().ctypes.get_as_parameter()
-
-    def get_normal_map_c(self):
-        """Get C array with normal vectors map."""
-        warn('Normal maps are drawn by shaders', DeprecationWarning)
-        return self.get_normal_map().ctypes.get_as_parameter()
-
-    def get_normals(self):
-        """Get normal vectors for each vertex.
-
-        Normal vector calculated for each triangle.
-        Normal vector of each vertex is normalized to |n| = 1.0
-        sum of normal vectors of triangles, which contain this point.
-        """
-        warn('Normal maps are calculated by shaders', DeprecationWarning)
-        if self.__normals is not None:
-            return self.__normals
-
-        self.__normals = get_normals(self.__vertices,
-                                     self.__triangles_flattened)
-        return self.__normals
+        if coefficients is None:
+            self.__coefficients = array([], dtype='f')
+        else:
+            self.__coefficients = coefficients
 
     @property
     def directed_light(self):
@@ -168,87 +53,19 @@ class Face:
 
     @property
     def ambient_light(self):
-        return self.__constant_light
+        return self.__ambient_light
 
     @ambient_light.setter
     def ambient_light(self, ambient_light):
-        self.__constant_light = ambient_light
+        self.__ambient_light = ambient_light
 
     @property
     def light(self):
         return concatenate((self.directed_light, [self.ambient_light]))
 
     @property
-    def normal_min(self):
-        warn('Normal map will not be used', DeprecationWarning)
-        return self.__normal_min
-
-    @normal_min.setter
-    def normal_min(self, normal_min):
-        warn('Normal map will not be used', DeprecationWarning)
-        self.__normal_min = normal_min
-
-    @property
-    def normal_max(self):
-        warn('Normal map will not be used', DeprecationWarning)
-        return self.__normal_max
-
-    @normal_max.setter
-    def normal_max(self, normal_max):
-        warn('Normal map will not be used', DeprecationWarning)
-        self.__normal_max = normal_max
-
-    @property
     def coefficients(self):
         return self.__coefficients
-
-    def get_normal_map(self):
-        """Get normal map for each vertex.
-
-        Normal map contains componentwise normalized to [0; 1]
-        normal vectors of each vertex to be represented as colors.
-        """
-        warn('Normal maps are drawn by shaders', DeprecationWarning)
-        if self.__normal_map is not None:
-            return self.__normal_map
-
-        self.__normal_map = self.get_normals().copy()
-
-        self.__normal_min = apply_along_axis(numpy.min, 0, self.__normal_map)
-        self.__normal_map -= self.__normal_min
-
-        self.__normal_max = apply_along_axis(numpy.max, 0, self.__normal_map)
-        self.__normal_map /= self.__normal_max
-
-        self.__normal_map = self.__normal_map.astype('f')
-
-        return self.__normal_map
-
-    def normal_map_to_normal_vectors(self, normal_map):
-        """Convert normal map to normal vectors.
-
-        Image RGB channels cannot contain negative values,
-        but it's necessary to have normal vectors of each point
-        for fitting procedure.
-        This method accepts normal map of the Face
-        and performs denormalization.
-        """
-        warn('Normal maps will not be used', DeprecationWarning)
-        return (normal_map * self.__normal_max) + self.__normal_min
-
-    def set_light(self, directed_light=None, constant_light=None):
-        """Change light conditions of the Face."""
-        warn('Use properties instead', DeprecationWarning)
-        if directed_light is not None:
-            directed_light = array(directed_light)
-            if directed_light.shape != (3,):
-                raise ValueError(ERROR_TEXT['LIGHT_DIRECTION']
-                                 .format(directed_light.shape))
-            self.__directed_light = directed_light
-        if constant_light is not None \
-                and constant_light > -2 and constant_light < 2:
-            self.__constant_light = constant_light
-        self.__light_map = None
 
     @staticmethod
     def set_triangles(triangles=None, triangles_c=None):
