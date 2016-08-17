5f7e12d41f1c80c67e2360ae7091838672a3eb13
char-lie <5962274+char-lie@users.noreply.github.com>
2016-08-17 11:19:28 +0300

    Removed deprecated estimate_light method from Model Fitter

--- src/fitter/ModelFitter.py
+++ src/fitter/ModelFitter.py
@@ -28,51 +28,6 @@ class ModelFitter:
                 ambient_light=0.5
             )
 
-    def estimate_light(self, normals):
-        """Estimates light parameters to the image with given normal vectors.
-
-        Light parameters contain vector of directed light
-        and intensity of ambient light.
-        """
-        warn('New shadows model', DeprecationWarning)
-        indices = nonzero(normals[:, 3])
-
-        N = normals[indices]
-        Y = self.__image[indices]
-
-        N_x = N[:, 0]
-        N_y = N[:, 1]
-        N_z = N[:, 2]
-
-        y_x = Y.dot(N_x)
-        y_y = Y.dot(N_y)
-        y_z = Y.dot(N_z)
-
-        n_x = N_x.sum()
-        n_y = N_y.sum()
-        n_z = N_z.sum()
-
-        n_xx = N_x.dot(N_x)
-        n_xy = N_x.dot(N_y)
-        n_xz = N_x.dot(N_z)
-        n_yy = N_y.dot(N_y)
-        n_yz = N_y.dot(N_z)
-        n_zz = N_z.dot(N_z)
-
-        A = array([
-            [n_xx, n_xy, n_xz, n_x],
-            [n_xy, n_yy, n_yz, n_y],
-            [n_xz, n_yz, n_zz, n_z],
-            [n_x, n_y, n_z, len(N)]
-        ])
-        y = array([y_x, y_y, y_z, Y.sum()])
-
-        x, _, _, _ = lstsq(A, y)
-        # Fails `test_estimate_light` test, because matrix cannot be inverted.
-        # x = inv(A).dot(y)
-
-        return x
-
     def start(self):
         """Start fitting procedure.
 
