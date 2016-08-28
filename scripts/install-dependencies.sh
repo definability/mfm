eada20e67e35445074b851acc723ba1986c4323d
char-lie <5962274+char-lie@users.noreply.github.com>
2016-08-28 16:55:16 +0300

    Added installation dependencies for Pillow library

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,6 +2,7 @@
 
 apt update
 apt install -y python-opengl python-numpy python-scipy
+apt install -y libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev libffi-dev
 
 pip install --upgrade pip
 pip install scipy numpy pyopengl ipython pillow
