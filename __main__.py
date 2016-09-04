3fb1aa0f1346c4eb1628eeab8590d8b13361b3cb
char-lie <5962274+char-lie@users.noreply.github.com>
2016-09-04 23:44:36 +0300

    Getting list of fitters from configuration file

--- __main__.py
+++ __main__.py
@@ -25,33 +25,13 @@ view = View((500, 500))
 model = Model(view)
 model_input = ModelInput(model)
 
+fitters = fitting_settings['fitters']
+
 model_filename = get_datafile_path('test.png')
 image = Image.open(model_filename).convert('L')
 original_data = array((image.getdata())).astype('f') / 255
 image_data = original_data.reshape(image.size)[::-1, :].flatten()
 image.close()
-
-fitters = [{
-    'fitter': 'BruteForce',
-    'dimensions': 0,
-    'steps': [1, 10, 10, 5],
-    'levels': list(range(4)),
-    'offsets': [0, -0.5, -0.5, 0],
-    'scales': [0, -2, -2, 1]
-}, {
-    'fitter': 'BruteForce',
-    'dimensions': 4,
-    'steps': [8, 8, 8, 8],
-    'levels': list(range(0, 4)),
-    'offsets': [-0.5, -0.5, -0.5, -0.5],
-    'scales': [8, 8, 8, 8]
-}, {
-    'fitter': 'BGD',
-    'dimensions': 50,
-    'max_loops': 100,
-    'dx': 1.,
-    'step': 50.
-}]
 
 chain = FittersChain(fitters, image_data, model)
 
