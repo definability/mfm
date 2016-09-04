f9bf04730522dade510bf83da37d28d2d0c73b04
char-lie <5962274+char-lie@users.noreply.github.com>
2016-09-04 13:27:57 +0300

    Added final callback to Model Fitter

--- src/fitter/ModelFitter.py
+++ src/fitter/ModelFitter.py
@@ -8,7 +8,8 @@ class ModelFitter:
 
     Should call Face rendering and wait for response.
     """
-    def __init__(self, image, dimensions=199, model=None, initial_face=None):
+    def __init__(self, image, dimensions=199, model=None, initial_face=None,
+                 callback=None):
         """Initializes fitter for given image.
 
         Fits provided number of dimensions of given model to the image.
@@ -16,6 +17,7 @@ class ModelFitter:
         self.__image = array(image)
         self.__model = model
         self._dimensions = dimensions
+        self.__callback = callback
 
         self._initial_face = initial_face
         if self._initial_face is None:
@@ -31,6 +33,14 @@ class ModelFitter:
         Should be called by host.
         """
         raise NotImplementedError()
+
+    def finish(self, face):
+        """Finish fitting procedure.
+
+        Should be called by Fitter when fitting is finished.
+        """
+        if self.__callback:
+            self.__callback(face)
 
     def request_face(self, face, label=None):
         """Requests rendered face with given parameters.
