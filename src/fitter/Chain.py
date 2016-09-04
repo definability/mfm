bc87779f8e4178f5b76aea69382303df6c70127a
char-lie <5962274+char-lie@users.noreply.github.com>
2016-09-04 12:31:32 +0300

    Moved fitters parser from static method to function in Chain

--- src/fitter/Chain.py
+++ src/fitter/Chain.py
@@ -3,6 +3,17 @@ from .ModelFitter import ModelFitter
 import src.fitter
 
 
+def parse_fitter(fitter):
+    """Get Fitter class from input parameter."""
+    if isinstance(fitter, ModelFitter):
+        return fitter
+    elif fitter in src.fitter.__all__:
+        return src.fitter.__dict__[fitter]
+    elif fitter + 'Fitter' in src.fitter.__all__:
+        return src.fitter.__dict__[fitter + 'Fitter']
+    raise NotImplementedError()
+
+
 class FittersChain:
     """Class for Face fitters chaining."""
     def __init__(self, chain, image, model, dimensions=199):
@@ -12,19 +23,8 @@ class FittersChain:
         for node in chain:
             self.__parameters.append(
                 {key: value for key, value in node.items() if key != 'fitter'})
-            self.__fitters.append(FittersChain.parse_fitter(node['fitter']))
+            self.__fitters.append(parse_fitter(node['fitter']))
 
     def start(self, callback):
         """Start fitting procedure."""
-        raise NotImplementedError()
-
-    @staticmethod
-    def parse_fitter(fitter):
-        """Get Fitter class from input parameter."""
-        if isinstance(fitter, ModelFitter):
-            return fitter
-        elif fitter in src.fitter.__all__:
-            return src.fitter.__dict__[fitter]
-        elif fitter + 'Fitter' in src.fitter.__all__:
-            return src.fitter.__dict__[fitter + 'Fitter']
         raise NotImplementedError()
