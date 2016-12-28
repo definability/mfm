9594e789819df090acdf1a9c65827f36422493d9
char-lie <5962274+char-lie@users.noreply.github.com>
2016-12-28 22:15:31 +0200

    Added doc comments to shaders module

--- shaders/__init__.py
+++ shaders/__init__.py
@@ -1,7 +1,9 @@
+"""Utilities for work with shaders files."""
 from os.path import dirname, abspath, join
 
 SHADERS_FOLDER = dirname(abspath(__file__))
 
 
 def get_shader_path(filename):
+    """Get full path to shader file."""
     return join(SHADERS_FOLDER, filename)
