defc284397ef983d27c52be8067e3827d317b297
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-12-28 20:44:04 +0200

    Made 'config' parameter optional
    Sometimes application is launched only to see faces and no fitters config needed

--- __main__.py
+++ __main__.py
@@ -17,7 +17,7 @@ LOGGER_LEVELS = [logging.getLevelName(i*LOGGER_LEVELS_STEP)
 parser = argparse.ArgumentParser(
     description='Morphable Face Model fitting application')
 parser.add_argument(
-    '--config', metavar='config', type=str, required=True,
+    '--config', metavar='config', type=str, required=False,
     help='specify configuration file for fitting procedure')
 parser.add_argument('-l', '--log', help='set logger level', type=str.upper,
                     default='WARNING', choices=LOGGER_LEVELS)
@@ -29,32 +29,35 @@ logging.basicConfig(
     format=' '.join(['[%(asctime)s]', '[%(levelname)8s]',
                      '[%(module)s:%(funcName)s]', '%(message)s']))
 
-fitting_settings = None
-with open(args.config) as config:
-    fitting_settings = json.load(config)
-
-fitters = fitting_settings['fitters']
-
-face_parameters = fitting_settings['input'].get('initial_face', {})
-
-coefficients = face_parameters.get('coefficients', [])
-directed_light = face_parameters.get('directed_light', (0., 0., 0.))
-ambient_light = face_parameters.get('ambient_light', 0.)
-initial_face = Face(coefficients=coefficients,
-                    directed_light=directed_light,
-                    ambient_light=ambient_light)
-
-model_filename = get_datafile_path(fitting_settings['input']['input_image'])
-image = Image.open(model_filename).convert('L')
-original_data = array(image.getdata()).astype('f') / 255
-image_data = original_data.reshape(image.size)[::-1, :].flatten()
-image.close()
-
 MFM.init()
 view = View((500, 500))
 model = Model(view)
 model_input = ModelInput(model)
 
-chain = FittersChain(fitters, image_data, model, initial_face=initial_face)
+if args.config is None:
+    model.start()
+else:
+    fitting_settings = None
+    with open(args.config) as config:
+        fitting_settings = json.load(config)
 
-model.start(chain)
+    fitters = fitting_settings['fitters']
+
+    face_parameters = fitting_settings['input'].get('initial_face', {})
+
+    coefficients = face_parameters.get('coefficients', [])
+    directed_light = face_parameters.get('directed_light', (0., 0., 0.))
+    ambient_light = face_parameters.get('ambient_light', 0.)
+    initial_face = Face(coefficients=coefficients,
+                        directed_light=directed_light,
+                        ambient_light=ambient_light)
+
+    face_filename = get_datafile_path(fitting_settings['input']['input_image'])
+    image = Image.open(face_filename).convert('L')
+    original_data = array(image.getdata()).astype('f') / 255
+    image_data = original_data.reshape(image.size)[::-1, :].flatten()
+    image.close()
+
+    chain = FittersChain(fitters, image_data, model, initial_face=initial_face)
+
+    model.start(chain)
