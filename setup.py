653615062a8af9dad640e5cbfa82eb5ca650523b
char-lie <5962274+char-lie@users.noreply.github.com>
2017-01-03 20:02:06 +0200

    Revert "Removed compilation of C modules from setup.py"
    This reverts commit 31777e5539bc54370fb53f991c9244fcaf7f9a93.
    
    Compilation of C modules for setuptools.
    Needed for issue #53

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from setuptools import Extension, setup, find_packages
 from os.path import join, dirname
+import numpy.distutils.misc_util
+
+SOURCES_DIR = 'libs'
+C_EXTENSION = '.c'
+WRAPPER_PREFIX = '_'
+
+def get_sources(name):
+    filename = name + C_EXTENSION
+    return [join(SOURCES_DIR, n) for n in [filename, WRAPPER_PREFIX + filename]]
+
+normals = Extension('src.normals', get_sources('normals'))
+face = Extension('src.face', get_sources('face'))
 
 long_description=''
 with open(join(dirname(__file__), 'README.md')) as f:
     long_description = f.read()
 
 setup(
+    ext_modules=[normals, face],
+    include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
     name='MFM',
     version='0.7.2',
     license='MIT',
