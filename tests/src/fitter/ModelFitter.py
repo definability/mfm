f7cf4f3771304d1041a8fab7b74e82a31d433ad4
Valeriy Krygin <valeriy.krygin@gmail.com>
2017-01-07 19:57:45 +0200

    Remade all unit tests to pytest

--- tests/src/fitter/ModelFitter.py
+++ tests/src/fitter/ModelFitter.py
@@ -1,10 +1,7 @@
-from unittest import TestCase
 from numpy import array
 
 from src.fitter import ModelFitter
 
 
-class ModelFitterTest(TestCase):
-
-    def test_constructor(self):
-        self.assertIsInstance(ModelFitter(array([])), ModelFitter)
+def test_constructor():
+    assert isinstance(ModelFitter(array([])), ModelFitter)
