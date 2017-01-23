8349fe18b9ebf7b676c00efba3864de42bf1f2d4
Valeriy Krygin <valeriy.krygin@gmail.com>
2017-01-23 23:18:38 +0200

    Removed Gibbs Sampler Fitter from imports in fitters module

--- src/fitter/__init__.py
+++ src/fitter/__init__.py
@@ -1,7 +1,6 @@
 from .FittersChain import FittersChain
 from .ModelFitter import ModelFitter
 from .NelderMeadFitter import NelderMeadFitter
-from .GibbsSamplerFitter import GibbsSamplerFitter
 from .BruteForceFitter import BruteForceFitter
 from .BGDFitter import BGDFitter
 from .MonteCarloFitter import MonteCarloFitter
