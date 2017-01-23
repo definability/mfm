b9bc265bfb86dca6ccbda3d45982f06021b60594
char-lie <5962274+char-lie@users.noreply.github.com>
2017-01-23 23:18:53 +0200

    Added Coordinate Descent fitter into fitters module

--- src/fitter/__init__.py
+++ src/fitter/__init__.py
@@ -4,8 +4,10 @@ from .NelderMeadFitter import NelderMeadFitter
 from .BruteForceFitter import BruteForceFitter
 from .BGDFitter import BGDFitter
 from .MonteCarloFitter import MonteCarloFitter
+from .CoordinateDescentFitter import CoordinateDescentFitter
 
 __all__ = ['FittersChain', 'ModelFitter',
            'NelderMeadFitter',
            'BruteForceFitter', 'BGDFitter',
-           'MonteCarloFitter']
+           'MonteCarloFitter',
+           'CoordinateDescentFitter']
