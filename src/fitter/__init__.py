de41fd596fb349f8697f57876444646bc22d8333
Valeriy Krygin <valeriy.krygin@gmail.com>
2017-02-20 18:30:14 +0200

    Removed Nelder-Mead fitter from list of available fitters

--- src/fitter/__init__.py
+++ src/fitter/__init__.py
@@ -1,11 +1,9 @@
 from .FittersChain import FittersChain
 from .ModelFitter import ModelFitter
-from .NelderMeadFitter import NelderMeadFitter
 from .BruteForceFitter import BruteForceFitter
 from .BGDFitter import BGDFitter
 from .CoordinateDescentFitter import CoordinateDescentFitter
 
 __all__ = ['FittersChain', 'ModelFitter',
-           'NelderMeadFitter',
            'BruteForceFitter', 'BGDFitter',
            'CoordinateDescentFitter']
