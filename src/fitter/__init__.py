5f42e470c0bec879ae2cf67ccd1fd26ec23eba71
Valeriy Krygin <valeriy.krygin@gmail.com>
2017-02-20 18:29:52 +0200

    Removed Monte-Carlo Fitter

--- src/fitter/__init__.py
+++ src/fitter/__init__.py
@@ -3,11 +3,9 @@ from .ModelFitter import ModelFitter
 from .NelderMeadFitter import NelderMeadFitter
 from .BruteForceFitter import BruteForceFitter
 from .BGDFitter import BGDFitter
-from .MonteCarloFitter import MonteCarloFitter
 from .CoordinateDescentFitter import CoordinateDescentFitter
 
 __all__ = ['FittersChain', 'ModelFitter',
            'NelderMeadFitter',
            'BruteForceFitter', 'BGDFitter',
-           'MonteCarloFitter',
            'CoordinateDescentFitter']
