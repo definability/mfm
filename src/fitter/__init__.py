8b55f35363d534f676ef6bfed71387b2b3be0356
Valeriy Krygin <valeriy.krygin@gmail.com>
2017-02-20 18:32:14 +0200

    Refactored fitters list

--- src/fitter/__init__.py
+++ src/fitter/__init__.py
@@ -4,6 +4,10 @@ from .BruteForceFitter import BruteForceFitter
 from .BGDFitter import BGDFitter
 from .CoordinateDescentFitter import CoordinateDescentFitter
 
-__all__ = ['FittersChain', 'ModelFitter',
-           'BruteForceFitter', 'BGDFitter',
-           'CoordinateDescentFitter']
+__all__ = [
+    'FittersChain',
+    'ModelFitter',
+    'BruteForceFitter',
+    'BGDFitter',
+    'CoordinateDescentFitter',
+]
